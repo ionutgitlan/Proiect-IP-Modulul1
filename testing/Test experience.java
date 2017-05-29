27929780d9c6c0e129d59143f2de71a41720eecd
dar-ungureanu <dar.ungureanu@gmail.com>
2017-05-30 00:28:32 +0300

    Update Test experience.java

--- testing/Test experience.java
+++ testing/Test experience.java
@@ -8,8 +8,9 @@ public class CVTest {
     public void testGetWorkExperienceValueGeneral() {
         String job = "I am a manager";
         int experienceYears = 10;
-        int expResult = 210;       
-        float result = CV.getWorkExperienceValue(job, experienceYears);
+        int expResult = 210;
+        String path="C:\\Users\\Dell\\Desktop\\top-jobs.txt";
+        float result = CV.getWorkExperienceValue(job, experienceYears,path);
         assertEquals(expResult, result, 0.0);
     }
     
@@ -18,7 +19,8 @@ public class CVTest {
         String job="I am a manager and a programmer";
         int experienceYears= 5;
         int expResult=340;
-        float result=CV.getWorkExperienceValue(job,experienceYears);
+        String path="C:\\Users\\Dell\\Desktop\\top-jobs.txt";
+        float result=CV.getWorkExperienceValue(job,experienceYears,path);
         assertEquals(expResult,result,0.0);
     }
     
@@ -27,7 +29,8 @@ public class CVTest {
         String job="Iamamanagerandaprogrammer";
         int experienceYears= 5;
         int expResult=340;
-        float result=CV.getWorkExperienceValue(job,experienceYears);
+        String path="C:\\Users\\Dell\\Desktop\\top-jobs.txt";
+        float result=CV.getWorkExperienceValue(job,experienceYears,path);
         assertEquals(expResult,result,0.0);
     }
     
@@ -36,7 +39,8 @@ public class CVTest {
         String job="I aM a maNaGer aNd A proGrAmMer";
         int experienceYears= 5;
         int expResult=340;
-        float result=CV.getWorkExperienceValue(job,experienceYears);
+        String path="C:\\Users\\Dell\\Desktop\\top-jobs.txt";
+        float result=CV.getWorkExperienceValue(job,experienceYears,path);
         assertEquals(expResult,result,0.0);
     }
     
@@ -45,7 +49,8 @@ public class CVTest {
         String job="I am a painter";
         int experienceYears= 12;
         int expResult=12;
-        float result=CV.getWorkExperienceValue(job,experienceYears);
+        String path="C:\\Users\\Dell\\Desktop\\top-jobs.txt";
+        float result=CV.getWorkExperienceValue(job,experienceYears,path);
         assertEquals(expResult,result,0.0);
     }
     
@@ -53,8 +58,9 @@ public class CVTest {
     public void testGetWorkExperienceValueEmpty() {
         String job = "";
         int experienceYears = 0;
-        int expResult = 0;       
-        float result = CV.getWorkExperienceValue(job, experienceYears);
+        int expResult = 0;
+        String path="C:\\Users\\Dell\\Desktop\\top-jobs.txt";
+        float result = CV.getWorkExperienceValue(job, experienceYears,path);
         assertEquals(expResult, result, 0.0);
     }
     
